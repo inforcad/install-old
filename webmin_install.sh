fb81f7386bc494dd0d6f6fd4556b2e013a147676
Antonio Jiménez <antonio@inforcad.es>
2017-07-21 07:28:15 +0200

    Update webmin_install.sh

--- webmin_install.sh
+++ webmin_install.sh
@@ -10,7 +10,7 @@
 #--------------------------------------------------
 echo -e "\n---- Installing webmin ----"
 sudo echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
-sudo cd /root
+cd /root
 sudo wget http://www.webmin.com/jcameron-key.asc
 sudo apt-key add jcameron-key.asc
 sudo apt-get update
