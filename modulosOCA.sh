0717dc3120abe3a5fd72d2ef3f10106985804384
Antonio Jiménez <antonio@inforcad.es>
2017-08-03 10:32:34 +0200

    Update modulosOCA.sh

--- modulosOCA.sh
+++ modulosOCA.sh
@@ -32,27 +32,30 @@ sudo git clone -b $VERSION https://github.com/OCA/pos.git $DESCARGAS/pos
 sudo git clone -b $VERSION https://github.com/OCA/bank-statement-import.git $DESCARGAS/bank-statement-import
 
 echo -e "\n---- Create some links ----"
-sudo ln -s $DESCARGAS/account-financial-reporting/account_export_csv $ADDONS
-sudo ln -s $DESCARGAS/account-financial-reporting/account_move_line_report_xls $ADDONS
-sudo ln -s $DESCARGAS/account-financial-tools/account_chart_update $ADDONS
-sudo ln -s $DESCARGAS/account-financial-tools/account_invoice_constraint_chronology $ADDONS
-sudo ln -s $DESCARGAS/account-financial-tools/account_invoice_currency $ADDONS
-sudo ln -s $DESCARGAS/account-financial-tools/account_journal_always_check_date $ADDONS
-sudo ln -s $DESCARGAS/account-financial-tools/account_renumber $ADDONS
-sudo ln -s $DESCARGAS/account-payment/account_due_list $ADDONS
-sudo ln -s $DESCARGAS/account-payment/account_due_list_payment_mode $ADDONS
+sudo ln -s $DESCARGAS/account-financial-reporting/account_export_csv/ $ADDONS
+sudo ln -s $DESCARGAS/account-financial-reporting/account_move_line_report_xls/ $ADDONS
+sudo ln -s $DESCARGAS/account-financial-reporting/account_tax_balance/ $ADDONS
+sudo ln -s $DESCARGAS/account-financial-tools/account_chart_update/ $ADDONS
+sudo ln -s $DESCARGAS/account-financial-tools/account_invoice_constraint_chronology/ $ADDONS
+sudo ln -s $DESCARGAS/account-financial-tools/account_invoice_currency/ $ADDONS
+sudo ln -s $DESCARGAS/account-financial-tools/account_journal_always_check_date/ $ADDONS
+sudo ln -s $DESCARGAS/account-financial-tools/account_renumber/ $ADDONS
+sudo ln -s $DESCARGAS/account-payment/account_due_list/ $ADDONS
+sudo ln -s $DESCARGAS/account-payment/account_due_list_payment_mode/ $ADDONS
 sudo ln -s $DESCARGAS/bank-payment/account_banking_mandate/ $ADDONS 
 sudo ln -s $DESCARGAS/bank-payment/account_banking_pain_base/ $ADDONS
 sudo ln -s $DESCARGAS/bank-payment/account_banking_payment_export/ $ADDONS
 sudo ln -s $DESCARGAS/bank-payment/account_banking_payment_transfer/ $ADDONS
 sudo ln -s $DESCARGAS/bank-payment/account_banking_sepa_direct_debit/ $ADDONS
 sudo ln -s $DESCARGAS/bank-payment/account_direct_debit/ $ADDONS
-sudo ln -s $DESCARGAS/bank-payment/account_payment_partner $ADDONS
+sudo ln -s $DESCARGAS/bank-payment/account_payment_partner/ $ADDONS
+sudo ln -s $DESCARGAS/bank-payment/account_payment_mode/ $ADDONS
 sudo ln -s $DESCARGAS/bank-statement-import/account_bank_statement_import/ $ADDONS
 sudo ln -s $DESCARGAS/knowledge/attachment_preview/ $ADDONS
 sudo ln -s $DESCARGAS/knowledge/attachments_to_filesystem/ $ADDONS
 sudo ln -s $DESCARGAS/knowledge/document_page/ $ADDONS
 sudo ln -s $DESCARGAS/knowledge/document_url/ $ADDONS
+sudo ln -s $DESCARGAS/knowledge/knowledge/ $ADDONS
 sudo ln -s $DESCARGAS/l10n-spain/account_balance_reporting_xls/ $ADDONS
 sudo ln -s $DESCARGAS/l10n-spain/account_balance_reporting/ $ADDONS
 sudo ln -s $DESCARGAS/l10n-spain/account_refund_original $ADDONS
@@ -83,6 +86,8 @@ sudo ln -s $DESCARGAS/reporting-engine/report_custom_filename/ $ADDONS
 sudo ln -s $DESCARGAS/reporting-engine/report_xls/ $ADDONS
 sudo ln -s $DESCARGAS/server-tools/disable_openerp_online/ $ADDONS
 sudo ln -s $DESCARGAS/server-tools/mass_editing/ $ADDONS
+sudo ln -s $DESCARGAS/server-tools/date_range/ $ADDONS
+sudo ln -s $DESCARGAS/server-tools/disable_odoo_online/ $ADDONS
 sudo ln -s $DESCARGAS/web/web_export_view/ $ADDONS
 
 echo "Done! Odoo OCA modules installed"
